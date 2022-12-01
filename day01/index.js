3a7499a6bc1b5d8bb643847ca6d550f28eae5add
floriangolling <florian.golling@epitech.eu>
2022-12-01 11:51:57 +0100

    feat: console.log :)

--- day01/index.js
+++ day01/index.js
@@ -1,1 +1,1 @@
-require('fs').readFileSync('input.txt', "utf-8").split('\r\n\r\n').map((x) => x.split('\n')).map((x) => x.map(t => parseInt(t))).map((x => x.reduce((a, b) => a + b, 0))).sort((a, b) => b - a).reduce((a, b, i, d) => i == 1 ? console.log(d[0], d[0] + d[1] + d[2]): 0)
+require('fs').readFileSync('input.txt', "utf-8").split('\r\n\r\n').map((x) => x.split('\n')).map((x) => x.map(t => parseInt(t))).map((x => x.reduce((a, b) => a + b, 0))).sort((a, b) => b - a).reduce((a, b, i, d) => i == 1 ? console.log(`first exercice: ${d[0]}`, `second exercice ${d[0] + d[1] + d[2]}`): 0)
